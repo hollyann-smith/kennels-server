e340affb88705a3bdca4e4b94f2ae9cc0e141990
Holly Smith <136410573+hollyann-smith@users.noreply.github.com>
2024-04-15 20:20:54 -0500

    Add files via upload

---
+++ kennels-server/views/employee_requests.py
@@ -0,0 +1,28 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Jenna Solis"
+    }
+]
+
+# Function with a single parameter
+def get_single_employee(id):
+    """Gets Single employee
+    """
+    # Variable to hold the found employee, if it exists
+    requested_employee = None
+
+    # Iterate the employeeS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
+
+def get_all_employees():
+    """FUCK THIS
+    """
+    return EMPLOYEES
